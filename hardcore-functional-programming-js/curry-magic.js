b0be71ca24ed4bfb5dd2bd07d447e6d2c9b8f30c
bruno-vinicius <bvsgoncalves@gmail.com>
2016-03-31 10:06:46 -0300

    Understaning the curry function

--- hardcore-functional-programming-js/curry-magic.js
+++ hardcore-functional-programming-js/curry-magic.js
@@ -1,9 +1,17 @@
 function curry(fn){
+  //Step 2: Return the fuction for curry
   return function(){
-    if(fn.length > arguments.length){
+	//Step 4: Executing the function
+    if(fn.length > arguments.length) {
      var slice = Array.prototype.slice;
+	 //Step 4.1: Trasnforming arguments in an array.
+	 //Because arguments object it is similar to an Array, but does not have any Array properties except length.
      var args = slice.apply(arguments);
-     return function(){
+	 //Step 4.2: Return the function that our array.map will execute
+     return function() {
+		 //Step 5: Execute our mapping function on each element of an array,
+		 //this function it's a closure, so it remember our args (name) that was passed early in execution
+		 //and concat with the arguments [currentValue, index, Array] given by the map function.
        return fn.apply(
          null, args.concat(slice.apply(arguments)));
      };
@@ -13,10 +21,12 @@ function curry(fn){
   }
 }
 
-var get = curry(function(prop, obj){
+//Step 1: Calling curry for currying our function.
+var get = curry(function getWithoutMagic(prop, obj){
   return obj[prop];
 });
 
-var people = [{'name': 'Bruno'},{'name': 'Sofia'}];
+var people = [{'name': 'Bruno', 'age': 25},{'name': 'Sofia', 'age': 23}];
 
+//Step 3: Mapping an array of people for get name property
 var names = people.map(get('name'));
